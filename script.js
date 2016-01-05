2e2566a8bf4bce75dcb42f6e33566538ef6dbf96
ghrehh <gjburns9@hotmail.com>
2016-01-05 21:28:27 +0000

    sdfsdf

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-
-
 $(document).ready(function(){
-  var music_anchor = $('.music_anchor').offset();
-  var info_anchor = $('.info_anchor').offset();
-  var gigs_anchor = $('.gigs_anchor').offset();
-  var contact_anchor = $('.contact_anchor').offset();
+  var music_anchor = $('.music-anchor').offset();
+  var info_anchor = $('.info-anchor').offset();
+  var gigs_anchor = $('.gigs-anchor').offset();
+  var contact_anchor = $('.contact-anchor').offset();
   
   
-  $("#music-button").click(function(){
+  $(".music-button").click(function(){
+    console.log("poo")
     $(window).scrollTop(music_anchor.top);
   });
-  $("#info-button").click(function(){
+  $(".info-button").click(function(){
     $(window).scrollTop(info_anchor.top);
   });
-  $("#gigs-button").click(function(){
+  $(".gigs-button").click(function(){
     $(window).scrollTop(gigs_anchor.top);
   });
-  $("#contact-button").click(function(){
+  $(".contact-button").click(function(){
     $(window).scrollTop(contact_anchor.top);
   });
     
